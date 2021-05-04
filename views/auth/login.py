d84a1ed28cee7f2b6a049905eeac93e05f729e27
AndriCoder95 <60884328+AndriCoder95@users.noreply.github.com>
2021-05-04 13:44:14 +0200

    more comments on login part

--- views/auth/login.py
+++ views/auth/login.py
@@ -8,24 +8,31 @@ from flask import (
     url_for,
     g,
 )
+
+# needed for decoding password hashes. User class is needed too.
 from werkzeug.security import check_password_hash
 from database.models.user import User
 
+# Blueprint for login route.
 login_route = Blueprint("login", __name__, url_prefix="/auth")
 
-
+# login page (prefix: /auth). Frontend is based on layout.html and login.html in templates folder
 @login_route.route("/login", methods=("GET", "POST"))
 def login():
     user_id = session.get("user_id")
+    # Check if user is already logged in, redirect to home page if this is the case
     if user_id:
         return redirect(url_for("index"))
+    # check if URL was visited with input from the form (login attempt)
     if request.method == "POST":
+        # assign form input from login.html to variables
         data = request.form
         email = data.get("email")
         password = data.get("password")
 
         error = None
 
+        # check if user exists and if password matches with the decoded password from database.
         if not email or not password:
             error = "email or password not provided"
         else:
@@ -36,16 +43,17 @@ def login():
             elif not check_password_hash(user.password, password):
                 error = "Incorrect password."
 
+            # if user is found in database and the password is correct: redirect to homepage and start a session
             if error is None:
                 session.clear()
                 session["user_id"] = user.id
                 return redirect(url_for("index"))
 
         flash(error)
-
+    # display login page when url is visited (without POST method from form)
     return render_template("auth/login.html")
 
-
+# keep track of user information
 @login_route.before_app_request
 def load_logged_in_user():
     user_id = session.get("user_id")
@@ -55,7 +63,7 @@ def load_logged_in_user():
     else:
         g.user = User.query.filter_by(id=user_id).first()
 
-
+# logout route, visited when logout button in navbar is clicked. session (cookies) get cleared.
 @login_route.route("/logout", methods=["GET"])
 def logout():
     session.clear()
